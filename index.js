21f61e242c179f5145f54b12cb013bd3939b3600
masashi.g <masashi.g@gmail.com>
2020-12-24 22:21:25 +0900

    add speak test

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const TTGoogleHome = require('./text-to-googlehome');
 
 
+// --- English ---
 const textEng = 'Hello World'
-const urlEng =  TTGoogleHome.getUrlfromText(textEng, null);
+const urlEng = TTGoogleHome.getUrlfromText(textEng, null);
 console.log(textEng, urlEng);
 
+// --- Japanese ---
 const textJp = 'こんにちは'
-const urlJp =  TTGoogleHome.getUrlfromText(textJp, 'ja');
+const urlJp = TTGoogleHome.getUrlfromText(textJp, 'ja');
 console.log(textJp, urlJp);
---
+++ test/test_speak.js
@@ -0,0 +1,43 @@
+//
+// node test/test_speak.js deviceHost [text [lang]]
+//
+
+"use strict"
+
+const TTGoogleHome = require('../text-to-googlehome');
+
+let host = null;
+let text = 'テストです';
+let lang = 'ja'
+
+// --- get args ---
+if (process.argv.length > 2) {
+  host = process.argv[2];
+}
+else {
+  console.error('ERROR: need deviceHost');
+  process.exit(1);
+}
+
+if (process.argv.length > 3) {
+  text = process.argv[3];
+}
+if (process.argv.length > 4) {
+  lang = process.argv[4];
+}
+
+console.warn(host, text, lang);
+
+// --- speak ---
+TTGoogleHome.speakToDeviceAsync(host, text, lang)
+  .then(e => {
+    // -- OK --
+    console.warn('speak [%s] to %s --> ', text, host, e);
+    process.exit(0);
+  })
+  .catch(err => {
+    // -- error --
+    console.error('speak ERROR:', err);
+    process.exit(1);
+  });
+
